67016ce1f698fd792fd3506053a34d4ab7b5d801
saracaitlyn-cruz11 <b2029scacruz@pshs.edu.ph>
2026-01-15 10:35:31 +0800

    save

---
+++ public/q3_2gjava.js
@@ -0,0 +1,11 @@
+function changeColor(element){
+
+    console.log (element);
+    element.style.backgroundColor="beige";
+}
+
+function resetColor(element){
+
+    console.log(element);
+    element.style.backgroundColor="light red";
+}
